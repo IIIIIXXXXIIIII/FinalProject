3cf7906bc29100373b9c365f21997f302a0e947d
user910120 <user910120@74-80-245-38.ic2net.net>
2018-05-01 20:54:00 -0700

    Stuck on data passing section and tableview populating Added MapKit Section of application

---
+++ Final/Restaurants.swift
@@ -0,0 +1,25 @@
+//
+//  Restaurants.swift
+//  Final
+//
+//  Created by user910120 on 5/1/18.
+//  Copyright © 2018 user910120. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class Restaurants: NSObject {
+    
+    let restaurauntTitle = String?
+    let restaurantType = String
+    let coordinate: CLLocationCoordinate2D
+    
+    init(title: String, type: String, coordinate: CLLocationCoordinate2D ) {
+        self.restaurauntTitle = title
+        self.restaurantType = type
+        self.coordinate = coordinate
+        
+        super.init()
+    }
+}
--- Final/SecondViewController.swift
+++ Final/SecondViewController.swift
@@ -7,11 +7,36 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
+
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let intialLocation = CLLocation(latitude: 21.329099, longitude: -158.088889)
+    
+    let regionRadius = CLLocationDistance = 1000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.title = ["Map"]
+        
+        centerMapOnLocation(location: initialLocation)
+        
+        let restaurantOne = Restaurant(title: "Aloha Salads", type: "Local", coordinate: CLLocationCoordinate2D (latitude: 21.3829, longitude: -157.9400))
+        let restaurantTwo = Restaurant(title: "Dennys", type: "American", coordinate: CLLocationCoordinate2D (latitude: 21.3293, longitude: -158.0923))
+        let restaurantThree = Restaurant(title: "Outback Steakhouse", type: "Australian", coordinate: CLLocationCoordinate2D (latitude: 21.3284, longitude: -158.0869))
+        let restaurantFour = Restaurant(title: "Panda Express", type: "American Oriental", coordinate: CLLocationCoordinate2D (latitude: 21.3291, longitude: -158.0865))
+        let restaurantFive = Restaurant(title: "Ramen-Ya", type: "Japanese", coordinate: CLLocationCoordinate2D (latitude: 21.33350, longitude: -158.0900))
+        let restaurantSix = Restaurant(title: "Wendys", type: "American", coordinate: CLLocationCoordinate2D (latitude: 21.3307, longitude: -158.0859))
+        
+    }
+        func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate.regionRadius)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
